b915b792cd65340ea25db6d895ce45b0f6f28229
JunichiroKohari <junichiro@kohari.jp>
2020-05-05 08:55:21 +0900

    カード形式で記事一覧表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,66 @@
 <template>
   <v-container fluid>
-    <template v-if="posts.length">
-      <ul v-for="(post, i) in posts" :key="i">
-      {{post.fields.image.fields.file}}
-        <li>{{ post.fields.title }}</li>
-        <ul>
-          <v-img
-            :src="post.fields.image.fields.file.url"
-            :alt="post.fields.image.fields.file.title"
-            :aspect-ratio="16/9"
-            max-width="400"
-            max-height="225"/>
-          <li>{{ post.fields.body }}</li>
-          <li>{{ post.fields.publishDate }}</li>
-        </ul>
-      </ul>
-    </template>
-    <template v-else>
-      投稿された記事はありません。
-    </template>
+    <v-row
+      justify="center"
+    >
+      <v-col
+        cols="12"
+        sm="11"
+        md="10"
+        xl="8"
+      >
+        <v-row v-if="posts.length">
+          <v-col
+            v-for="(post, i) in posts"
+            :key="i"
+            cols="12"
+            sm="6"
+            lg="4"
+            xl="3"
+          >
+            <v-card
+              max-width="400"
+              class="mx-auto"
+            >
+              <v-img
+                :src="post.fields.image.fields.file.url"
+                :alt="post.fields.image.fields.title"
+                :aspect-ratio="16/9"
+                max-height="200"
+                class="white--text"
+              >
+                <v-card-title class="align-end fill-height font-weight-bold">
+                  {{ post.fields.title }}
+                </v-card-title>
+              </v-img>
+
+              <v-card-text>
+                {{ post.fields.publishDate }}
+              </v-card-text>
+
+              <v-list-item three-line style="min-height: unset;">
+                <v-list-item-subtitle>
+                  {{ post.fields.body }}
+                </v-list-item-subtitle>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-spacer />
+                <v-btn
+                  text
+                  color="primary"
+                >
+                  この記事をみる
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+        <div v-else class="text-center">
+          投稿された記事はありません。
+        </div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
