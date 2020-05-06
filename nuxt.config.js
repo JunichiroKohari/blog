f8cc0c8fc78609e829333e60aa7223bf3d4ef2a3
JunichiroKohari <junichiro@kohari.jp>
2020-05-06 12:30:25 +0900

    add_generate_property

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 import colors from 'vuetify/es5/util/colors'
-require('dotenv').config();
+require('dotenv').config()
+const client = require('./plugins/contentful').default
 
 export default {
   mode: 'universal',
@@ -50,6 +51,21 @@ export default {
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
     CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID,
     CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN
+  },
+  generate: {
+    routes() {
+      return Promise.all([
+        client.getEntries({
+          content_type: process.env.CTF_BLOG_POST_TYPE_ID
+        })
+      ]).then(([ posts ]) => {
+        return [
+          ...posts.items.map(post => {
+            return { route: `posts/${post.fields.slug}`, payload: post }
+          })
+        ]
+      })
+    }
   },
   /*
   ** vuetify module configuration
