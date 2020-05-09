e7962d03a6d821bc40d55764f490271cef19b67f
JunichiroKohari <junichiro@kohari.jp>
2020-05-09 17:40:28 +0900

    タグ一覧ページ追加

--- components/ui/breadcrumbs.vue
+++ components/ui/breadcrumbs.vue
@@ -3,8 +3,10 @@
     <template #item="props">
       <v-breadcrumbs-item
         :to="props.item.to"
+        active-class=""
+        exact-active-class="v-breadcrumbs__item--disabled"
       >
-        <v-icon v-if="props.item.icon" color="primary">
+        <v-icon v-if="props.item.icon" :color="iconColor(props.item)">
           {{ props.item.icon }}
         </v-icon>
         <span class="ml-1">
@@ -34,6 +36,11 @@ export default {
   computed: {
     breadcrumbs() {
       return this.items.concat(this.addItems)
+    },
+    iconColor() {
+      return (item) => {
+        return item.iconColor || 'primary'
+      }
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -68,7 +68,7 @@
                   <v-chip
                     v-for="(tag) in post.fields.tags"
                     :key="tag.sys.id"
-                    to="#"
+                    :to="linkTo('tags', tag)"
                     small
                     label
                     outlined
