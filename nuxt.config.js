57f89ffed2a82d3e7db53cf1954841440ec372ad
JunichiroKohari <junichiro@kohari.jp>
2020-05-29 20:59:44 +0900

    ダークモードOFF

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,11 +89,11 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
+          accent: colors.orange.lighten5,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
           warning: colors.amber.base,
